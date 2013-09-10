b9ce8e86dc15fb92ead152c383d0b458b2ede833
hands-rec <hands.records@gmail.com>
2013-09-10 20:37:18 +0900

    initial

---
+++ mysql_table_to_csv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+user=$1
+password=$2
+host=$3
+database=$4
+
+show_table_file="./tables.csv"
+
+# show tables
+echo "show tables"
+sql_show_tables="show tables;"
+mysql -u${user} -p${password} -h${host} -A ${database} -N -e "${sql_show_tables}" > ${show_table_file}
+
+# show full columns from xxx
+dir_tables="tables"
+rm -rf ${dir_tables}
+mkdir ${dir_tables}
+sql_show_full_columns="show full columns from "
+
+for table in `cat ${show_table_file}`
+do
+	echo ${table}
+	mysql -u${user} -p${password} -h${host} -A ${database} -e "${sql_show_full_columns} ${table}" | sed -e 's/,/ /g' | sed -e 's/\t/,/g' > ${dir_tables}/${table}".csv"
+done
+
